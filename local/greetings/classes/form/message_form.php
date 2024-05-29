110968a4cdd391728b944d269fe3e6608ed019d5
Chef Service Informatique <engn.si.2022@gmail.com>
2024-05-29 18:03:07 +0100

    Create a form and handle form processing.

--- local/greetings/classes/form/message_form.php
+++ local/greetings/classes/form/message_form.php
@@ -23,15 +23,26 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+namespace local_greetings\form;
+
 defined('MOODLE_INTERNAL') || die();
 
+require_once($CFG->libdir . '/formslib.php');
 
-$string['greetingloggedinuser'] = 'Greetings, {$a}.';
-$string['greetinguser'] = 'Greetings, user.';
+/**
+ * Form specification to get text message from the user
+ */
+class message_form extends \moodleform {
+    /**
+     * Define the form.
+     */
+    public function definition() {
+        $mform = $this->_form; // Don't forget the underscore!
 
-$string['greetinguserar'] = 'تحيات, {$a}.';
-$string['greetinguserfr'] = 'Salutations, {$a}.';
+        $mform->addElement('textarea', 'message', get_string('yourmessage', 'local_greetings')); // Add elements to your form.
+        $mform->setType('message', PARAM_TEXT); // Set type of element.
 
-$string['pluginname'] = 'Greetings';
-
-$string['yourmessage'] = 'Your message';
+        $submitlabel = get_string('submit');
+        $mform->addElement('submit', 'submitmessage', $submitlabel);
+    }
+}
--- local/greetings/index.php
+++ local/greetings/index.php
@@ -32,8 +32,18 @@ $PAGE->set_pagelayout('standard');
 $PAGE->set_title(get_string("pluginname", "local_greetings"));
 $PAGE->set_heading(get_string("pluginname", "local_greetings"));
 
+$messageform = new \local_greetings\form\message_form();
+
 echo $OUTPUT->header();
 
 echo '<h3>' . local_greetings_get_greeting($USER) . '</h3>';
+
+$messageform->display();
+
+if ($data = $messageform->get_data()) {
+    $message = required_param('message', PARAM_TEXT);
+    echo $OUTPUT->heading($message, 4);
+    var_dump($data);
+}
 
 echo $OUTPUT->footer();
