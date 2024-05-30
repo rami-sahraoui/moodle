9a0ad5e1a9c94fcea08958fe96754404bdaa9e54
Chef Service Informatique <engn.si.2022@gmail.com>
2024-05-30 14:19:23 +0100

    Record author of post in the greetings table.

--- local/greetings/version.php
+++ local/greetings/version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_greetings';
 $plugin->release = '0.1.0';
-$plugin->version = 2024052700;
+$plugin->version = 2024052701;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
